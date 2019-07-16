6fcc2cf41c582d209c579a07a53b429137a49a8d
Luciano <luchosr@gmail.com>
2019-07-16 19:59:24 -0300

    fix modal styles

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,20 +7,28 @@ const Modal = props => {
 
   const closeModal = event => {
     event.stopPropagation();
+    // event.preventDefault();
     props.closeModal();
   };
   return (
     <div className="modal" onClick={closeModal} style={divStyle}>
       <div className="modal-content" onClick={event => event.stopPropagation()}>
-        <span className="close" onClick={closeModal}>
-          &times;
-        </span>
         <form className="todo-form" onSubmit={props.onSubmit}>
           <h2>Add Item</h2>
           <input type="text" value={props.value} onChange={props.onChange} />
-          <button onClick={closeModal} onSubmit={props.onSubmit}>
-            Submit
-          </button>
+          <div className="buttons">
+            {" "}
+            <button onClick={closeModal} className="close">
+              Close
+            </button>
+            <button
+              className="add"
+              onSubmit={props.onSubmit}
+              onClick={closeModal}
+            >
+              Add
+            </button>
+          </div>
         </form>
       </div>
     </div>
